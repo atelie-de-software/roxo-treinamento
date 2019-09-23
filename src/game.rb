e18f55a2326841e1b6312111f1081e383b6c6c80
Webgoal <webgoal@Webgoals-iMac.local>
2019-09-23 19:13:16 -0300

    Implementa o metodo direita

--- src/game.rb
+++ src/game.rb
@@ -17,4 +17,17 @@ class Game
   def tela
     @tela
   end
+
+  def direita
+    @tela =
+    "w w w w     \n" +
+    "w w w w     \n" +
+    "w w w w     \n" +
+    "w w w w     \n" +
+    "w w w w     \n" +
+    "            \n" +
+    "            \n" +
+    "            \n" +
+    "  A          "
+  end
 end
