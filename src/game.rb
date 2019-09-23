289fa955e46fccdc1ed3d336d6cb24a64017a2ae
Webgoal <webgoal@Webgoals-iMac.local>
2019-09-23 19:10:56 -0300

    Implementa a tela inicial

--- src/game.rb
+++ src/game.rb
@@ -1,13 +1,20 @@
 class Game
-  def tela
-    "w w w w w w \n" +
-    "w w w w w w \n" +
-    "w w w w w w \n" +
-    "w w w w w w \n" +
-    "w w w w w w \n" +
+  attr :tela
+
+  def initialize()
+    @tela =
+    "w w w w     \n" +
+    "w w w w     \n" +
+    "w w w w     \n" +
+    "w w w w     \n" +
+    "w w w w     \n" +
     "            \n" +
     "            \n" +
     "            \n" +
     "A            "
+  end
+
+  def tela
+    @tela
   end
 end
