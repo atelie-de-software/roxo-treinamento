677d40fed5c0088ac8821619ee8917743e57252b
Raphael Martins <raphael@webgoal.com.br>
2019-09-23 19:23:50 -0300

    teste do inimigo destruido

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -221,4 +221,25 @@ RSpec.describe Game do
 
     expect(game.tela).to eq tela_esperada
   end
+
+  xit 'inimigo destruido' do
+    game.tiro
+    game.tick
+    game.tick
+    game.tick
+    game.tick
+
+    tela_esperada =
+      "w w w w     \n" +
+      "w w w w     \n" +
+      "w w w w     \n" +
+      "w w w w     \n" +
+      "  w w w     \n" +
+      "            \n" +
+      "            \n" +
+      "            \n" +
+      "A           "
+
+    expect(game.tela).to eq tela_esperada
+  end
 end
