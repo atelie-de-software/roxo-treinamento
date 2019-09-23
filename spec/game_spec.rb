f2564e6b9a9881c49a6dd935333b338960c08e63
Raphael Martins <raphael@webgoal.com.br>
2019-09-23 19:22:53 -0300

    teste do tiro acertando o inimigo

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -201,4 +201,24 @@ RSpec.describe Game do
 
     expect(game.tela).to eq tela_esperada
   end
+
+  xit 'tiro acertando o inimigo' do
+    game.tiro
+    game.tick
+    game.tick
+    game.tick
+
+    tela_esperada =
+      "w w w w     \n" +
+      "w w w w     \n" +
+      "w w w w     \n" +
+      "w w w w     \n" +
+      "* w w w     \n" +
+      "            \n" +
+      "            \n" +
+      "            \n" +
+      "A           "
+
+    expect(game.tela).to eq tela_esperada
+  end
 end
