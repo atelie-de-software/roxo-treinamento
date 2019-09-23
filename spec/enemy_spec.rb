1e81e2122de8be474ea9d95e0ae05855f64f4930
Webgoal <webgoal@Webgoals-iMac.local>
2019-09-23 17:57:08 -0300

    Retorna se o inimigo esta vivo

--- spec/enemy_spec.rb
+++ spec/enemy_spec.rb
@@ -2,10 +2,27 @@ require './src/enemy'
 
 RSpec.describe Enemy do
   describe "method #show" do
-    subject { Enemy.new.show }
+    subject { Enemy.new(true).show }
 
     it "return caracter" do
       expect(subject).to eq 'W'
+    end
+  end
+
+  describe "method #alive?" do
+    subject { Enemy.new(alive).alive? }
+    context 'when the enemy is alive' do
+      let(:alive) { true }
+      it "return true" do
+        expect(subject).to be_truthy
+      end
+    end
+    context 'when the enemy is dead' do
+      let(:alive) { false }
+
+      it "return false" do
+        expect(subject).to be_falsey
+      end
     end
   end
 end
--- src/enemy.rb
+++ src/enemy.rb
@@ -1,4 +1,14 @@
 class Enemy
+  attr :alive
+
+  def initialize(alive)
+    @alive = alive
+  end
+
+  def alive?
+    @alive
+  end
+
   def show
     'W'
   end
