a2cf41a76f7a311a692592de10dd13c951c7f057
Raphael Martins <raphael@webgoal.com.br>
2019-09-24 18:42:45 -0300

    Atualizando mais rapido

--- main.rb
+++ main.rb
@@ -4,18 +4,16 @@ require './src/game.rb'
 # draw app and redraw after each keystroke
 Dispel::Screen.open do |screen|
   jogo = Game.new
-  screen.draw "SpaceInv\n\n" + jogo.tela + "\n~~~~~~~~~"
 
-  Dispel::Keyboard.output timeout: 0.5 do |key|
+  Dispel::Keyboard.output timeout: 0.2 do |key|
+    jogo.tick
+    screen.draw "SpaceInv\n\n" + jogo.tela + "\n~~~~~~~~~"
+
     next          unless key
 
     exit(true)    if key == :"Ctrl+c"
     jogo.direita  if key == :right
     jogo.esquerda if key == :left
     jogo.tiro     if key == ' '
-    next          if key == :timeout
-
-    jogo.tick
-    screen.draw "SpaceInv\n\n" + jogo.tela + "\n~~~~~~~~~"
   end
 end
