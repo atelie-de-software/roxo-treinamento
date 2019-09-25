564c73766e4aa2c818e7ad4386904f2af956de62
João Paulo <joao@webgoal.com.br>
2019-09-25 17:24:34 -0300

    Cria tabuleiro em modo gráfico

--- main.rb
+++ main.rb
@@ -4,17 +4,28 @@ require './src/game.rb'
 jogo = Game.new
 set width: 800, height: 800, background: '#000000'
 
-tick = 0
+sprites = {
+  'w': 'images/monster.jpg',
+  'A': 'images/spaceship.jpg',
+  '|': 'images/tiro.jpg',
+  ' ': 'images/background.jpg',
+  '*': 'images/explosion.jpg'
+}
 
 update do
   system "clear"
+  clear
   jogo.tick
   tela = jogo.tela
   puts tela
-  Image.new('images/monster.jpg', x: 0, y: 0, width: 50, height: 50)
-  Image.new('images/tiro.jpg', x: 50, y: 0, width: 50, height: 50)
+
+  rows = tela.split("\n")
+  rows.each_with_index do |row, y|
+    row.each_char.with_index do |char, x|
+      Image.new(sprites[char.to_sym], x: (x * 50), y: (y * 50), width: 50, height: 50)
+    end
+  end
   sleep 0.2
-  tick += 1
 end
 
 on :key_down do |event|
