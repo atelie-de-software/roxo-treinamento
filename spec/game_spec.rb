78e1fbd92d4d0a982d592844a8a70862cc932f13
João Paulo <joao@webgoal.com.br>
2019-09-25 18:30:17 -0300

    renomeia game para jogo

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,7 +1,7 @@
 require './src/game'
 
 RSpec.describe Game do
-  let!(:game) { Game.new }
+  let!(:jogo) { Game.new }
 
   it 'tela inicial' do
     tela_inicial =
@@ -15,11 +15,11 @@ RSpec.describe Game do
       "            \n" +
       "A           "
 
-    expect(game.tela).to eq tela_inicial
+    expect(jogo.tela).to eq tela_inicial
   end
 
   it 'mover para a direita' do
-    game.direita
+    jogo.direita
 
     tela_esperada =
       "w w w w     \n" +
@@ -32,12 +32,12 @@ RSpec.describe Game do
       "            \n" +
       "  A         "
 
-    expect(game.tela).to eq tela_esperada
+    expect(jogo.tela).to eq tela_esperada
   end
 
   it 'mover duas vezes para a direita' do
-    game.direita
-    game.direita
+    jogo.direita
+    jogo.direita
 
     tela_esperada =
       "w w w w     \n" +
@@ -50,13 +50,13 @@ RSpec.describe Game do
       "            \n" +
       "    A       "
 
-    expect(game.tela).to eq tela_esperada
+    expect(jogo.tela).to eq tela_esperada
   end
 
   it 'mover duas vezes para a direita e uma vez para a esquerda' do
-    game.direita
-    game.direita
-    game.esquerda
+    jogo.direita
+    jogo.direita
+    jogo.esquerda
 
     tela_esperada =
       "w w w w     \n" +
@@ -69,11 +69,11 @@ RSpec.describe Game do
       "            \n" +
       "  A         "
 
-    expect(game.tela).to eq tela_esperada
+    expect(jogo.tela).to eq tela_esperada
   end
 
   it 'mover uma vez para a esquerda' do
-    game.esquerda
+    jogo.esquerda
 
     tela_esperada =
       "w w w w     \n" +
@@ -86,11 +86,11 @@ RSpec.describe Game do
       "            \n" +
       "A           "
 
-    expect(game.tela).to eq tela_esperada
+    expect(jogo.tela).to eq tela_esperada
   end
 
   it 'nave da um tiro' do
-    game.tiro
+    jogo.tiro
 
     tela_esperada =
       "w w w w     \n" +
@@ -102,12 +102,12 @@ RSpec.describe Game do
       "            \n" +
       "            \n" +
       "A           "
-    expect(game.tela).to eq tela_esperada
+    expect(jogo.tela).to eq tela_esperada
   end
 
   it 'anda para a direita e da um tiro' do
-    game.direita
-    game.tiro
+    jogo.direita
+    jogo.tiro
 
     tela_esperada =
       "w w w w     \n" +
@@ -120,11 +120,11 @@ RSpec.describe Game do
       "            \n" +
       "  A         "
 
-    expect(game.tela).to eq tela_esperada
+    expect(jogo.tela).to eq tela_esperada
   end
 
   it 'tiro e um tick' do
-    game.tiro
+    jogo.tiro
 
     tela_esperada =
       "w w w w     \n" +
@@ -137,9 +137,9 @@ RSpec.describe Game do
       "            \n" +
       "A           "
 
-    expect(game.tela).to eq tela_esperada
+    expect(jogo.tela).to eq tela_esperada
 
-    game.tick
+    jogo.tick
 
     tela_esperada =
       "w w w w     \n" +
@@ -152,11 +152,11 @@ RSpec.describe Game do
       "|           \n" +
       "A           "
 
-    expect(game.tela).to eq tela_esperada
+    expect(jogo.tela).to eq tela_esperada
   end
 
   it 'tiro e dois ticks' do
-    game.tiro
+    jogo.tiro
 
     tela_esperada =
       "w w w w     \n" +
@@ -169,9 +169,9 @@ RSpec.describe Game do
       "            \n" +
       "A           "
 
-    expect(game.tela).to eq tela_esperada
+    expect(jogo.tela).to eq tela_esperada
 
-    game.tick
+    jogo.tick
 
     tela_esperada =
       "w w w w     \n" +
@@ -184,9 +184,9 @@ RSpec.describe Game do
       "|           \n" +
       "A           "
 
-    expect(game.tela).to eq tela_esperada
+    expect(jogo.tela).to eq tela_esperada
 
-    game.tick
+    jogo.tick
 
     tela_esperada =
       "w w w w     \n" +
@@ -199,15 +199,15 @@ RSpec.describe Game do
       "            \n" +
       "A           "
 
-    expect(game.tela).to eq tela_esperada
+    expect(jogo.tela).to eq tela_esperada
   end
 
   it 'tiro acertando o inimigo' do
-    game.tiro
-    game.tick
-    game.tick
-    game.tick
-    game.tick
+    jogo.tiro
+    jogo.tick
+    jogo.tick
+    jogo.tick
+    jogo.tick
 
     tela_esperada =
       "w w w w     \n" +
@@ -220,16 +220,16 @@ RSpec.describe Game do
       "            \n" +
       "A           "
 
-    expect(game.tela).to eq tela_esperada
+    expect(jogo.tela).to eq tela_esperada
   end
 
   xit 'inimigo destruido' do
-    game.tiro
-    game.tick
-    game.tick
-    game.tick
-    game.tick
-    game.tick
+    jogo.tiro
+    jogo.tick
+    jogo.tick
+    jogo.tick
+    jogo.tick
+    jogo.tick
 
     tela_esperada =
       "w w w w     \n" +
@@ -242,18 +242,18 @@ RSpec.describe Game do
       "            \n" +
       "A           "
 
-    expect(game.tela).to eq tela_esperada
+    expect(jogo.tela).to eq tela_esperada
   end
 
   xit 'destroi o inimigo e anda para a direita' do
-    game.tiro
-    game.tick
-    game.direita
-    game.tick
-    game.direita
-    game.tick
-    game.tick
-    game.tick
+    jogo.tiro
+    jogo.tick
+    jogo.direita
+    jogo.tick
+    jogo.direita
+    jogo.tick
+    jogo.tick
+    jogo.tick
 
     tela_esperada =
       "w w w w     \n" +
@@ -266,13 +266,13 @@ RSpec.describe Game do
       "            \n" +
       "    A       "
 
-    expect(game.tela).to eq tela_esperada
+    expect(jogo.tela).to eq tela_esperada
   end
 
   xit 'atira e anda para a direita' do
-    game.tiro
-    game.tick
-    game.direita
+    jogo.tiro
+    jogo.tick
+    jogo.direita
 
     tela_esperada =
       "w w w w     \n" +
@@ -285,16 +285,16 @@ RSpec.describe Game do
       "|           \n" +
       "  A         "
 
-    expect(game.tela).to eq tela_esperada
+    expect(jogo.tela).to eq tela_esperada
   end
 
   xit 'atira e anda para a direita' do
-    game.tiro
-    game.tick
-    game.direita
-    game.tick
-    game.tick
-    game.tick
+    jogo.tiro
+    jogo.tick
+    jogo.direita
+    jogo.tick
+    jogo.tick
+    jogo.tick
 
     tela_esperada =
       "w w w w     \n" +
@@ -307,17 +307,17 @@ RSpec.describe Game do
       "            \n" +
       "  A         "
 
-    expect(game.tela).to eq tela_esperada
+    expect(jogo.tela).to eq tela_esperada
   end
 
   xit 'atira, mata o inimigo e os outros inimigos começam a se mover para a direita' do
-    game.tiro
-    game.tick
-    game.tick
-    game.tick
-    game.tick
-    game.tick
-    game.tick
+    jogo.tiro
+    jogo.tick
+    jogo.tick
+    jogo.tick
+    jogo.tick
+    jogo.tick
+    jogo.tick
 
     tela_esperada =
       "  w w w w   \n" +
@@ -330,7 +330,7 @@ RSpec.describe Game do
       "            \n" +
       "A           "
 
-    expect(game.tela).to eq tela_esperada
+    expect(jogo.tela).to eq tela_esperada
   end
 
 end
